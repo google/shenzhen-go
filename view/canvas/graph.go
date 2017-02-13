b66051dff57f16ad6bcd98796e2ec0ee2b9dfd0f
Josh Deprez <josh.deprez@gmail.com>
2017-02-13 21:11:15 +1100

    More experimenting

--- view/canvas/graph.go
+++ view/canvas/graph.go
@@ -20,6 +20,7 @@ package main
 
 import (
 	"math"
+	"math/rand"
 
 	"github.com/gopherjs/gopherjs/js"
 )
@@ -27,48 +28,74 @@ import (
 const (
 	fillStyle   = "#0099ff"
 	strokeStyle = "#dddddd"
+	snapLen     = 225
 )
 
 var (
-	graphCanvas = js.Global.Get("document").Call("getElementById", "graph-canvas")
-	ctx         = graphCanvas.Call("getContext", "2d")
+	graphCanvas   = js.Global.Get("document").Call("getElementById", "graph-canvas")
+	ctx           = graphCanvas.Call("getContext", "2d")
+	width, height int
+	on            bool
 )
 
+type point struct{ x, y int }
+
+func resize(*js.Object) {
+	on = false
+	width, height = graphCanvas.Get("clientWidth").Int(), graphCanvas.Get("clientHeight").Int()
+	graphCanvas.Set("width", width)
+	graphCanvas.Set("height", height)
+}
+
 func main() {
-	graphCanvas.Set("width", graphCanvas.Get("clientWidth").Int())
-	graphCanvas.Set("height", graphCanvas.Get("clientHeight").Int())
+	resize(nil)
+	js.Global.Get("window").Call("addEventListener", "resize", resize)
 	canvasRect := graphCanvas.Call("getBoundingClientRect")
-	width, height := graphCanvas.Get("width").Int(), graphCanvas.Get("height").Int()
 
-	on := false
 	startX, startY := 0, 0
+	snap := make([]point, 50)
+	for i := range snap {
+		snap[i] = point{rand.Intn(width), rand.Intn(height)}
+	}
 
-	graphCanvas.Set("onmousedown", func(this *js.Object) {
-		startX = this.Get("clientX").Int() - canvasRect.Get("left").Int()
-		startY = this.Get("clientY").Int() - canvasRect.Get("top").Int()
-		on = true
+	drawPoints := func() {
+		// Snap point
+		for _, p := range snap {
+			ctx.Call("beginPath")
+			ctx.Call("arc", p.x, p.y, 4, 0, 2*math.Pi, false)
+			ctx.Set("fillStyle", "#000")
+			ctx.Call("fill")
+			ctx.Set("lineWidth", 1)
+			ctx.Set("strokeStyle", strokeStyle)
+			ctx.Call("stroke")
+		}
+	}
+	drawPoints()
+
+	graphCanvas.Set("onmousedown", func(event *js.Object) {
+		startX = event.Get("clientX").Int() - canvasRect.Get("left").Int()
+		startY = event.Get("clientY").Int() - canvasRect.Get("top").Int()
+		for _, p := range snap {
+			if dx, dy := startX-p.x, startY-p.y; dx*dx+dy*dy < 100 {
+				startX, startY = p.x, p.y
+				on = true
+				break
+			}
+		}
 	})
 
-	drawScene := func(this *js.Object) {
-		if !on {
-			return
-		}
-		x := this.Get("clientX").Int() - canvasRect.Get("left").Int()
-		y := this.Get("clientY").Int() - canvasRect.Get("top").Int()
-
-		ctx.Call("clearRect", 0, 0, width, height)
-
+	drawLine := func(x1, y1, x2, y2 int) {
 		// Line outline
 		ctx.Call("beginPath")
-		ctx.Call("moveTo", startX, startY)
-		ctx.Call("lineTo", x, y)
+		ctx.Call("moveTo", x1, y1)
+		ctx.Call("lineTo", x2, y2)
 		ctx.Set("lineWidth", 4)
 		ctx.Set("strokeStyle", strokeStyle)
 		ctx.Call("stroke")
 
 		// Start dot
 		ctx.Call("beginPath")
-		ctx.Call("arc", startX, startY, 4, 0, 2*math.Pi, false)
+		ctx.Call("arc", x1, y1, 4, 0, 2*math.Pi, false)
 		ctx.Set("fillStyle", fillStyle)
 		ctx.Call("fill")
 		ctx.Set("lineWidth", 1)
@@ -77,7 +104,7 @@ func main() {
 
 		// End dot
 		ctx.Call("beginPath")
-		ctx.Call("arc", x, y, 4, 0, 2*math.Pi, false)
+		ctx.Call("arc", x2, y2, 4, 0, 2*math.Pi, false)
 		ctx.Set("fillStyle", fillStyle)
 		ctx.Call("fill")
 		ctx.Set("lineWidth", 1)
@@ -86,16 +113,45 @@ func main() {
 
 		// Line
 		ctx.Call("beginPath")
-		ctx.Call("moveTo", startX, startY)
-		ctx.Call("lineTo", x, y)
+		ctx.Call("moveTo", x1, y1)
+		ctx.Call("lineTo", x2, y2)
 		ctx.Set("lineWidth", 2)
 		ctx.Set("strokeStyle", fillStyle)
 		ctx.Call("stroke")
 	}
 
-	graphCanvas.Set("onmousemove", drawScene)
-	graphCanvas.Set("onmouseup", func(this *js.Object) {
-		drawScene(this)
+	graphCanvas.Set("onmousemove", func(event *js.Object) {
+		x := event.Get("clientX").Int() - canvasRect.Get("left").Int()
+		y := event.Get("clientY").Int() - canvasRect.Get("top").Int()
+		if !on {
+			return
+		}
+
+		ctx.Call("clearRect", 0, 0, width, height)
+		drawPoints()
+		for _, p := range snap {
+			if dx, dy := x-p.x, y-p.y; dx*dx+dy*dy < snapLen {
+				drawLine(startX, startY, p.x, p.y)
+				return
+			}
+		}
+		drawLine(startX, startY, x, y)
+	})
+
+	graphCanvas.Set("onmouseup", func(event *js.Object) {
+		x := event.Get("clientX").Int() - canvasRect.Get("left").Int()
+		y := event.Get("clientY").Int() - canvasRect.Get("top").Int()
+		if !on {
+			return
+		}
+		ctx.Call("clearRect", 0, 0, width, height)
+		drawPoints()
+		for _, p := range snap {
+			if dx, dy := x-p.x, y-p.y; -10 < dx && dx < 10 && -10 < dy && dy < 10 {
+				drawLine(startX, startY, p.x, p.y)
+				break
+			}
+		}
 		on = false
 	})
 }

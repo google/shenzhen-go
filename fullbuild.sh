09f9cfe897baac30406f17b3fe5bfd526b928bc0
Josh Deprez <josh.deprez@gmail.com>
2017-12-06 22:29:10 +1100

    (broken) Start down path of using gRPC-Web

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e
 set -u
-set -x
 cd $GOPATH/src/github.com/google/shenzhen-go
 
+# UI JS generation
 pushd view/svg
 go generate
 popd
+
+# gRPC generation
+protoc -I./proto shenzhen-go.proto --go_out=plugins=grpc:./api --gopherjs_out=plugins=grpc:./api
+# Since both implementations live in the same package, use tags to separate them.
+echo -e "//+build js\n$(cat api/shenzhen-go.pb.gopherjs.go)" > api/shenzhen-go.pb.gopherjs.go
+echo -e "//+build !js\n$(cat api/shenzhen-go.pb.go)" > api/shenzhen-go.pb.go
 
 go install github.com/google/shenzhen-go/cmd/shenzhen-go

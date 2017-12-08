ac462c7998042b0ca69e68a904096af7c5ebdae6
Josh Deprez <josh.deprez@gmail.com>
2017-12-08 19:21:33 +1100

    Fix build script

--- fullbuild.sh
+++ fullbuild.sh
@@ -3,17 +3,17 @@ set -u
 
 pushd $GOPATH/src/github.com/google/shenzhen-go
 
-# UI JS generation
-pushd ./view/svg
-go generate
-popd
-
 # gRPC generation
 rm ./proto/shenzhen-go.pb{,.gopherjs}.go
 protoc -I./proto shenzhen-go.proto --go_out=plugins=grpc:./proto --gopherjs_out=plugins=grpc:./proto
 # Since both implementations live in the same package, use tags to separate them.
 echo -e "//+build js\n$(cat ./proto/shenzhen-go.pb.gopherjs.go)" > ./proto/shenzhen-go.pb.gopherjs.go
 echo -e "//+build \x21js\n$(cat ./proto/shenzhen-go.pb.go)" > ./proto/shenzhen-go.pb.go
+
+# UI JS generation
+pushd ./view/svg
+go generate
+popd
 
 go install github.com/google/shenzhen-go/cmd/shenzhen-go
 

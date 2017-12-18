a61c0fae0b566c9fb8f76368b218c890e9ba2d70
Josh Deprez <josh.deprez@gmail.com>
2017-12-18 20:15:09 +1100

    Do the favicon the same way

--- server/view/generate.go
+++ server/view/generate.go
@@ -15,4 +15,5 @@
 package view
 
 //go:generate go run ../../scripts/embed.go -p view -v cssResources -o static-css.go css/*.css
+//go:generate go run ../../scripts/embed.go -p view -v imageResources -o static-images.go images/*
 //go:generate go run ../../scripts/embed.go -p view -v templateResources -o static-templates.go templates/*.html

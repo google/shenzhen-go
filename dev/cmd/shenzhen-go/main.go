1423c297b75db94d7d040c50db72347cb0c83f78
Josh Deprez <josh.deprez@gmail.com>
2018-04-28 17:51:54 +0200

    Add webview but hidden behind a flag for now

--- dev/cmd/shenzhen-go/main.go
+++ dev/cmd/shenzhen-go/main.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -27,6 +28,7 @@ import (
 	"time"
 
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/zserge/webview"
 	"google.golang.org/grpc"
 
 	pb "github.com/google/shenzhen-go/dev/proto/go"
@@ -36,9 +38,12 @@ import (
 
 const pingMsg = "Pong!"
 
-var uiAddr = flag.String("ui_addr", "localhost:8088", "Address to bind UI server to")
+var (
+	uiAddr            = flag.String("ui_addr", "localhost:0", "Address to bind UI server to")
+	useDefaultBrowser = flag.Bool("use_browser", true, "Load in the system's default web browser instead of the inbuilt webview")
+)
 
-func open(url string) error {
+func systemOpen(url string) error {
 	switch runtime.GOOS {
 	case "darwin":
 		return exec.Command("open", url).Run()
@@ -51,6 +56,10 @@ func open(url string) error {
 		fmt.Printf("Ready to open %s\n", url)
 		return nil
 	}
+}
+
+func webviewOpen(url string) error {
+	return webview.Open("SHENZHEN GO", url, 1152, 720, true)
 }
 
 func isUp(base string) bool {
@@ -66,14 +75,27 @@ func isUp(base string) bool {
 	return string(msg) == pingMsg
 }
 
-func openWhenUp(addr string) {
+func openWhenUp(addr net.Addr, useBrowser bool) {
 	base := fmt.Sprintf(`http://%s/`, addr)
-	t := time.NewTicker(100 * time.Millisecond)
-	defer t.Stop()
-	for range t.C {
-		if isUp(base) {
-			break
+	try := time.NewTicker(100 * time.Millisecond)
+	defer try.Stop()
+	timeout := time.NewTimer(5 * time.Second)
+	defer timeout.Stop()
+checkLoop:
+	for {
+		select {
+		case <-try.C:
+			if isUp(base) {
+				break checkLoop
+			}
+		case <-timeout.C:
+			fmt.Fprintf(os.Stderr, "Couldn't find server after 5 seconds, giving up")
+			os.Exit(1)
 		}
+	}
+	open := systemOpen
+	if !useBrowser {
+		open = webviewOpen
 	}
 	if err := open(base); err != nil {
 		fmt.Fprintf(os.Stderr, "Couldn't automatically open: %v\n", err)
@@ -96,11 +118,26 @@ func main() {
 	// Finally, all unknown paths are assumed to be files.
 	http.Handle("/", server.S)
 
+	ln, err := net.Listen("tcp", *uiAddr)
+	if err != nil {
+		log.Fatalf("net.Listen failed: %v", err)
+	}
+	defer ln.Close()
+
+	wait := make(chan struct{})
+	go func() {
+		if err := http.Serve(ln, nil); err != nil {
+			log.Fatalf("http.Serve failed: %v", err)
+		}
+		close(wait)
+	}()
+
 	// As soon as we're serving, launch "open" which should launch a browser,
 	// or ask the user to do so.
-	go openWhenUp(*uiAddr)
+	// This must be called from the main thread to avoid
+	// https://github.com/zserge/webview/issues/29.
+	openWhenUp(ln.Addr(), *useDefaultBrowser)
 
-	if err := http.ListenAndServe(*uiAddr, nil); err != nil {
-		log.Fatal(err)
-	}
+	// Job done.
+	<-wait
 }

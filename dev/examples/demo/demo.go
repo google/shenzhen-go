7759e836bc42bb7a6d8828ef2d8065a4f46de663
Josh Deprez <josh.deprez@gmail.com>
2018-05-29 00:36:33 +0000

    rm demo.go, regenerate generated.go

--- dev/examples/demo/demo.go
+++
@@ -1,9 +0,0 @@
-package demo
-
-import "fmt"
-
-// Demo is a function that reads from foo and writes 42 to bar.
-func Demo(foo <-chan int, bar chan<- int) {
-	fmt.Printf("Got %d, sending 42\n", <-foo)
-	bar <- 42
-}

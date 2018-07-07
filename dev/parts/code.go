9a154b256fd0a001b3b2782a3742263be59b187a
Josh Deprez <josh.deprez@gmail.com>
2018-07-07 11:48:26 +1000

    Update Code help

--- dev/parts/code.go
+++ dev/parts/code.go
@@ -55,23 +55,23 @@ var (
 			Name: "Help",
 			Editor: `<div>
 	<p>
-		A Code part runs (executes) any Go code that you write.
+		A Code part runs (executes) any Go code that you write. It is completely customisable.
 	</p><p>
-		It consists of 3 parts: a Head, a Body, and a Tail. 
-		The Head runs first, and only runs once, no matter what number Multiplicity is set to. 
-		The Body runs next. The number of concurrent copies of the Body that run is set by Multiplicity.
-		Finally, when all copies of the Body return, the Tail runs. 
+		The first two configuration panels are Pins and Imports. Pins configures the pins available,
+		and Imports configures the contents of the imports declaration needed for any code.
 	</p><p>
+		The actual Go code configuration consists of 3 parts: a Head, a Body, and a Tail.
+		First Head is run, then some number of instances of the Body, then the Tail.
+	</p><p>
+		Body is where the bulk of the code is usually written, as the Multiplicity parameter
+		causes multiple copies of the Body to be run. Multiplicity has no effect on Head or Tail.
 		The Head and Tail are useful for operations that should only be done once. For example, any 
 		output channels written to in the Body can be correctly closed (if desired) in the Tail.
 	</p><p>
-		Each instance of the Body can use the int parameters <code>instanceNumber</code> and <code>multiplicity</code>
-		to distinguish which instance is running and how many are running, if necessary. 
-		<code>0 <= instanceNumber < multiplicity</code>
-	</p><p>
-		Any channels referred to will automatically be detected and shown in the graph, and
-		when channels are renamed, these will be safely updated in the Code where they are
-		referred to.
+		Each instance of the Body can use the <code>int</code> parameters <code>instanceNumber</code>
+		and <code>multiplicity</code> to distinguish which instance is running and how many are running, 
+		if necessary. These parameters always satisfy the relation 
+		<code>0 <= instanceNumber < multiplicity</code>.
 	</p><p>
 		The <code>return</code> statement is allowed but optional in Code. There are no values that
 		need to be returned.
